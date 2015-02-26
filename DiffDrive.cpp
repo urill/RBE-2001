9bed0fccd8d3abf27b6936125d58959e76708b21
Keshuai Xu <i@xks.im>
2015-02-25 22:47:51 -0500

    fixed mirrored motor

--- DiffDrive.cpp
+++ DiffDrive.cpp
@@ -5,7 +5,7 @@ extern Servo rightWheel;
 void DiffDrive::go(int speed,int steer){
   int _speed = map(speed,-100,100,1000,2000);
   int _steer = map(steer,-200,200,-1000,1000);
-  int _leftOutput = constrain(_speed + _steer,1000,2000);
+  int _leftOutput = constrain(-_speed + _steer,1000,2000);
   int _rightOutput = constrain(_speed - _steer,1000,2000);
   leftWheel.writeMicroseconds(_leftOutput);
   rightWheel.writeMicroseconds(_rightOutput);
