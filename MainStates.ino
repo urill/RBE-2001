0fddaa07de0fde81b622dfc6b6a6208eed72d748
Keshuai Xu <i@xks.im>
2015-03-02 13:59:30 -0500

    more main sm

--- MainStates.ino
+++ MainStates.ino
@@ -133,76 +133,135 @@ State mainExtractingRodFromReactorState_2(){
 
 State mainExtractingRodFromReactorState_3(){
   if (elevatorSM.Finished){
-    sm.Set(mainRetractAndTurnAround_1);
+    sm.Set(mainRetractAndTurnAroundAtSpentReactorState_1);
   }
 }
 
-State mainRetractAndTurnAround_1(){
+State mainRetractAndTurnAroundAtSpentReactorState_1(){
   info("Retracting");
   moveSM.Set(retractState);
-  sm.Set(mainRetractAndTurnAround_2);
+  sm.Set(mainRetractAndTurnAroundAtSpentReactorState_2);
 }
 
-State mainRetractAndTurnAround_2(){
+State mainRetractAndTurnAroundAtSpentReactorState_2(){
   if (moveSM.Finished) {
     info("Turning around");
     moveSM.Set(turnAroundState);
-    sm.Set(mainMovingToSpentStorageState); // changed from   sm.Finish(); //TODO
-    
+    sm.Set(mainRetractAndTurnAroundAtSpentReactorState_3);
   }
+}
+
+State mainRetractAndTurnAroundAtSpentReactorState_3(){
+  if (moveSM.Finished) {
+    sm.Set(mainMovingToSpentStorageState);
+  }
+}
+
+byte getClosestSpentStorage(){
+    if (currentPosition == REACTOR_A){
+      if (bluetoothSpentAvailability & SPENT_4_MASK) return SPENT_4;
+      if (bluetoothSpentAvailability & SPENT_3_MASK) return SPENT_3;
+      if (bluetoothSpentAvailability & SPENT_2_MASK) return SPENT_2;
+      if (bluetoothSpentAvailability & SPENT_1_MASK) return SPENT_1;
+    } else if (currentPosition == REACTOR_B){
+      if (bluetoothSpentAvailability & SPENT_1_MASK) return SPENT_1;
+      if (bluetoothSpentAvailability & SPENT_2_MASK) return SPENT_2;
+      if (bluetoothSpentAvailability & SPENT_3_MASK) return SPENT_3;
+      if (bluetoothSpentAvailability & SPENT_4_MASK) return SPENT_4;
+    } else {
+      severe("unexpected position");
+    }
+
+    info("no storage available");
+    return 0;
 }
 
 State mainMovingToSpentStorageState(){
-  /*CODE THAT ALLOWS TESTING UNTIL BT IS DONE
-  */
    if (moveSM.Finished) {
-    info("Moving to Spent");
-    
-    setLineFollowStopCondition(1,0,0);
-    moveSM.Set(lineFollowState);
-    
-    moveSM.Set(turnLeftWaitState);
-    
-    setLineFollowStopCondition(0,1,1);
-    moveSM.Set(lineFollowState);
-    
-    sm.Set(mainInsertingRodToSpentStorageState);
+    byte nextPosition = getClosestSpentStorage();
+    if (nextPosition != 0){
+      navi.setNavigation(currentPosition, nextPosition);
+      currentPosition = nextPosition;
+      sm.Set(mainMovingToSpentStorageState_2);
+      info("Moving to Spent");
     }
+  }
 }
+
+State mainMovingToSpentStorageState_2(){
+  if (moveSM.Finished) {
+    boolean done = processNavigate();
+    if (done){
+      sm.Set(mainAlignToSpentStorageState);
+    }
+  }
+}
+
+State mainAlignToSpentStorageState(){
+  setLineFollowStopCondition(0,1,1);
+  moveSM.Set(lineFollowState);
+}
+
+State mainAlignToSpentStorageState_2(){
+  if (moveSM.Finished) {
+    sm.Set(mainInsertingRodToSpentStorageState);
+  }
+}
+
 State mainInsertingRodToSpentStorageState(){
   if (moveSM.Finished) {
     gripperSM.Set(gripperOpenState);
-    sm.Set(mainReleaseRodState);
+    sm.Set(mainReleaseRodToSpentStorageState);
   }
 }
-State mainReleaseRodState(){
+State mainReleaseRodToSpentStorageState(){
   if(gripperSM.Finished){
     elevatorSM.Set(elevatorDownState);
-    sm.Set(mainBackup);
+    sm.Set(mainBackupAtSpentStorageState);
   }
 }
-State mainBackup(){
+State mainBackupAtSpentStorageState(){
   if(elevatorSM.Finished){
     moveSM.Set(retractState);
-    sm.Set(mainElevatorUp);
+    sm.Set(mainElevatorUpAtSpentStorageState);
 
   }
 }
-State mainElevatorUp(){
+State mainElevatorUpAtSpentStorageState(){
   if(moveSM.Finished){
    gripperSM.Set(gripperCloseState);
    elevatorSM.Set(elevatorUpState);
-   sm.Set(mainPushInRod);    
+   sm.Set(mainPushInRodAtSpentStorageState);
   }
 }
 
-State mainPushInRod(){
+State mainPushInRodAtSpentStorageState(){
    if(elevatorSM.Finished){
     setLineFollowStopCondition(0,1,1);
     moveSM.Set(lineFollowState);
-    sm.Set(movingToNewStorageState);    
+    sm.Set(movingToNewStorageState);
   }
- }
+}
+
+State mainRetractAndTurnAroundAtSpentStorageState_1(){
+  info("Retracting");
+  moveSM.Set(retractState);
+  sm.Set(mainRetractAndTurnAroundAtSpentStorageState_2);
+}
+
+State mainRetractAndTurnAroundAtSpentStorageState_2(){
+  if (moveSM.Finished) {
+    info("Turning around");
+    moveSM.Set(turnAroundState);
+    sm.Set(mainRetractAndTurnAroundAtSpentStorageState_3);
+  }
+}
+
+State mainRetractAndTurnAroundAtSpentStorageState_3(){
+  if (moveSM.Finished) {
+    sm.Set(mainMovingToSpentStorageState);
+  }
+}
 
 State movingToNewStorageState(){
  if(moveSM.Finished){
